ea8a6ce934eb88a3fb0897bbd1137b41cfd9045a
1045-gondralavishnusai-b1 <67041923+1045-gondralavishnusai-b1@users.noreply.github.com>
2020-06-20 17:03:49 +0530

    Add files via upload

---
+++ Simple intrest.py
@@ -0,0 +1,5 @@
+p=float(input('enter the  p value\n'))
+t=float(input('enter the t value\n'))
+r=float(input('enter the r value\n'))
+si=(p*t*r)/100
+print(si)
---
+++ swap.py
@@ -0,0 +1,11 @@
+a=int(input('enter a value\n'))
+b=int(input('enter b value\n'))
+print('before swapping')
+print("a=",a)
+print('b=',b)
+temp=a
+a=b
+b=temp
+print('after swapping')
+print('a=',a)
+print('b=',b)
