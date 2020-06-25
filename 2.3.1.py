f883f99c521913ea5bc3f95fc1249d964cb0364f
1045-gondralavishnusai-b1 <67041923+1045-gondralavishnusai-b1@users.noreply.github.com>
2020-06-25 18:19:54 +0530

    Add files via upload

---
+++ 2.3.1.py
@@ -0,0 +1,4 @@
+import math
+r=float(input('enter the radius:\n'))
+ar=math.pi*r*r
+print("area of a circle is %2f" %ar)
---
+++ 2.3.5.py
@@ -0,0 +1,5 @@
+import random
+num1=random.randrange(1,10000,50)
+print(num1)
+num2=random.randrange(1,10000,50)
+print(num2)
